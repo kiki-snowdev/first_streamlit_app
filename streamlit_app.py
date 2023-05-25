6ae7e60c2da40cfbe0222388e9b011f4611a7e1e
kiki-snowdev <134369298+kiki-snowdev@users.noreply.github.com>
2023-05-25 13:52:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,7 +18,7 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon") #sends a request and gets a json response
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi") #sends a request and gets a json response
 # streamlit.text(fruityvice_response.json()) -- writes the json response to the screen
 
 # normalises the json response 
